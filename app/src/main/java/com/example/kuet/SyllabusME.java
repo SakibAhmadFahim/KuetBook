9e5e80269f19587c9f4a58878fbe5f39c03f2179
SakibFahim <sakibfahim1999@gmail.com>
2022-11-16 02:17:59 +0600

    KuetBook

---
+++ app/src/main/java/com/example/kuet/SyllabusME.java
@@ -0,0 +1,27 @@
+package com.example.kuet;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.webkit.WebView;
+
+import com.example.kuet.CustomWebViewClientaehCSE;
+import com.example.kuet.R;
+
+public class SyllabusME extends AppCompatActivity {
+
+    @SuppressLint({"SetJavaScriptEnabled", "MissingInflatedId"})
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_syllabus_civil);
+
+        CustomWebViewClientaehCSE client=new CustomWebViewClientaehCSE(this);
+        WebView webView = findViewById(R.id.webView);
+        webView.setWebViewClient(client);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl("file:///android_asset/ME.html");
+    }
+}
+
